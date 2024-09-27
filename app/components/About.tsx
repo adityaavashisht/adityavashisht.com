1233c8f2a2b7187e3755d56719816890950ebf14
adityavashisht9 <aditya.vashisht@accenture.com>
2024-09-26 21:33:39 -0400

    fixed eslint errors

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -2,15 +2,16 @@ export default function About() {
   return (
     <section className="flex flex-col gap-y-10 font-light text-sm">
       <h1 className="text-lg font-semibold">Aditya Vashisht</h1>
-      <p>I'm a Developer based in Toronto, ON.</p>
+      <p>{"I'm a Developer based in Toronto, ON."}</p>
       <p>
-        Currently, I'm a Frontend Lead at Accenture, crafting meaningful web
-        experiences with cutting-edge tech. These days, I've been neck-deep in
-        Next.js (so, of course, React), along with NestJS and TypeScript.
+        {
+          "Currently, I'm a Frontend Lead at Accenture, crafting meaningful web experiences with cutting-edge tech. These days, I've been neck-deep in Next.js (so, of course, React), along with NestJS and TypeScript."
+        }
       </p>
       <p>
-        When I'm not immersed in code, you can usually find me lifting weights
-        and occasionally feeling curious about the vastness of space.
+        {
+          "When I'm not immersed in code, you can usually find me lifting weights and occasionally feeling curious about the vastness of space."
+        }
       </p>
     </section>
   );
