a157764f5c09805eb6fcd4d8f16bf444f43ce409
adityavashisht9 <aditya.vashisht@accenture.com>
2024-09-29 16:39:18 -0400

    refactored location test

--- app/api/geolocation/route.ts
+++ app/api/geolocation/route.ts
@@ -2,8 +2,5 @@ import { geolocation } from "@vercel/functions";
 import { NextResponse, NextRequest } from "next/server";
 
 export function GET(NextRequest: NextRequest) {
-  const { city } = geolocation(NextRequest);
-  return NextResponse.json({
-    city: city,
-  });
+  return NextResponse.json(geolocation(NextRequest));
 }
