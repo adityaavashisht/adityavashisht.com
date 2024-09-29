50dc379207d7cc787aaf8139e1d17e88e17d9675
adityavashisht9 <aditya.vashisht@accenture.com>
2024-09-29 17:30:48 -0400

    refactored decoded location test

--- app/api/geolocation/route.ts
+++ app/api/geolocation/route.ts
@@ -1,11 +1,13 @@
 import { geolocation } from "@vercel/functions";
 import { NextResponse, NextRequest } from "next/server";
+import { headers } from "next/headers";
 
 export function GET(request: NextRequest) {
   const geo = geolocation(request);
-
+  const headerList = headers();
   return NextResponse.json({
     ...geo,
     city: decodeURIComponent(geo.city || ""),
+    headers: headerList,
   });
 }
