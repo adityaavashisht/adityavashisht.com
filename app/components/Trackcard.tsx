8d383f4ff6a95cff23760cdf27864f26f0bfafe6
adityavashisht9 <aditya.vashisht@accenture.com>
2024-10-06 18:19:25 -0400

    refactored audio wave, icon

--- app/components/Trackcard.tsx
+++ app/components/Trackcard.tsx
@@ -109,23 +109,6 @@ export default function TrackCard({
             <p className="text-sm text-muted">{artist}</p>
           </div>
         </div>
-        {isPlaying && (
-          <div className="music-bars flex gap-x-[0.13rem] h-[0.2rem]">
-            {Array(6)
-              .fill(null)
-              .map((_, index) => (
-                <div
-                  key={index}
-                  className="bar h-full w-[0.13rem] rounded-[0.03rem]"
-                  style={{
-                    background: `linear-gradient(180deg, ${
-                      dominantColors[0]
-                    }, ${dominantColors[dominantColors.length - 1]})`,
-                  }}
-                />
-              ))}
-          </div>
-        )}
       </div>
       <div className="flex justify-between items-center">
         <div className="flex space-x-1 pl-1 items-center">
@@ -141,7 +124,55 @@ export default function TrackCard({
               ? `Last played on ${formatDate(lastPlayedAt)}`
               : null}
           </span>
+          <svg
+            className="w-4 h-4"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <defs>
+              <linearGradient
+                id="gradientFill"
+                x1="0%"
+                y1="0%"
+                x2="0%"
+                y2="100%"
+              >
+                <stop
+                  offset="0%"
+                  style={{ stopColor: dominantColors[0], stopOpacity: 1 }}
+                />
+                <stop
+                  offset="100%"
+                  style={{
+                    stopColor: dominantColors[dominantColors.length - 1],
+                    stopOpacity: 1,
+                  }}
+                />
+              </linearGradient>
+            </defs>
+            <path
+              d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z"
+              fill="url(#gradientFill)"
+            />
+          </svg>
         </div>
+        {isPlaying && (
+          <div className="music-bars flex gap-x-1 h-[0.19rem] pr-1">
+            {Array(6)
+              .fill(null)
+              .map((_, index) => (
+                <div
+                  key={index}
+                  className="bar h-full w-[0.2rem] rounded-[0.03rem]"
+                  style={{
+                    background: `linear-gradient(180deg, ${
+                      dominantColors[0]
+                    }, ${dominantColors[dominantColors.length - 1]})`,
+                  }}
+                />
+              ))}
+          </div>
+        )}
       </div>
     </div>
   );
