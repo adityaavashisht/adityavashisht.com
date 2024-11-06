5d7b5589776fb1cbbb8d683731d31b2f3b5973fc
adityavashisht9 <aditya.vashisht@accenture.com>
2024-11-05 23:54:06 -0500

    fixed timezone

--- app/utils/formatDate.ts
+++ app/utils/formatDate.ts
@@ -1,12 +1,13 @@
 export default function formatDate(dateString: string): string {
   const date = new Date(dateString);
+  console.log(dateString);
   return new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
     month: "short",
     day: "numeric",
     hour: "2-digit",
     minute: "2-digit",
     hour12: true,
     timeZoneName: "short",
+    timeZone: "America/New_York",
   }).format(date);
 }
