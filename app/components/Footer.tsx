8dddb015c3d6ce6d1622b380f87dc6dc0c35508c
Aditya Vashisht <adityavashisht03@gmail.com>
2026-02-08 17:42:12 -0500

    updated border-radius

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -71,7 +71,7 @@ export default function Footer() {
           <p>Current visitor from:</p>
           <Suspense
             fallback={
-              <span className="w-20 h-3 bg-decoration animate-pulse rounded-md" />
+              <span className="w-20 h-3 bg-decoration animate-pulse rounded-[4px]" />
             }
           >
             <p className="font-light">
